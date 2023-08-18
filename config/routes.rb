33fd237ae7da97a23bc80a5ab17ae88622fc6d30
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-18 12:27:27 +0000

    [update]4-27controllerファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :books, only: [:new, :create, :index, :show]
+  
+  resources :users, only: [:show, :edit]
 
 get 'homes/about' => 'homes#about', as: :about
 

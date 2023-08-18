8a621c5720b0d98cfdb699a3a27f232169e3c663
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-18 14:14:31 +0000

    [update]4-27Bootstrapを使うために必要な記述を追加する

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
 
   resources :books, only: [:new, :create, :index, :show]
-  
+
   resources :users, only: [:show, :edit]
 
 get 'homes/about' => 'homes#about', as: :about

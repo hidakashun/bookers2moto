34cd2b81713bb3941f2a54885f960d8141a6b206
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-19 04:33:10 +0000

    [add]4-27headerに必要なリンクを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  #get 'homes/top'
   devise_for :users
   root to: "homes#top"
+
+  get 'homes/about' => 'homes#about', as: :about
 
   resources :books, only: [:new, :create, :index, :show]
 
   resources :users, only: [:show, :edit]
-
-get 'homes/about' => 'homes#about', as: :about
 
 end

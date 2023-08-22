b57fcb7ea3d6a267bb451ca23e39d82e4f28f6d1
EC2 Default User <ec2-user@ip-172-31-3-46.ap-southeast-2.compute.internal>
2023-08-22 10:51:19 +0000

    [update]4-27他人のユーザ情報編集画面に遷移できない

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]#他人のユーザ情報編集画面に遷移できないようにしてる（カリキュラムほかのユーザーからのアクセスを制限しよう）
   def index
     @users = User.all
     @user = current_user
@@ -13,10 +14,15 @@ class UsersController < ApplicationController
   end
 
   def edit
+    
+    
     @user = User.find(params[:id])
+    
   end
 
   def update
+    
+    
     @user = User.find(params[:id])
     if @user.update(user_params)
     flash[:notice] = "You have updated user successfully."
@@ -27,6 +33,13 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def is_matching_login_user#他人のユーザ情報編集画面に遷移できないようにしてる（カリキュラムほかのユーザーからのアクセスを制限しよう）
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
 
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
